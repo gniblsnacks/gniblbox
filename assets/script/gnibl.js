ee56a574a770eec1b27c9a77b5d50830dcbdeb4a
bilalnaseer01@gmail.com <bilalnaseer01@gmail.com>
2019-01-25 03:06:39 +1100

    Updated Gnibl

--- assets/script/gnibl.js
+++ assets/script/gnibl.js
@@ -232,50 +232,6 @@ function pricingFunctions() {
   }
 }
 
-//my code
-var box_size = "starter box", delivery_frequency = "month", custom_box = false;
-function pricingFunctions() {
-  if ($('main').first().hasClass('pricing')) {
-    updatePricingHTML();
-    updateProductLink();
-    boxSliderInit();
-    $(".size-selection").find(".btn-tab").each(function() {
-      $(this).click(function() {
-        $(".size-selection").find(".selected-tab").removeClass('selected-tab');
-        $(this).addClass('selected-tab');
-        box_size = $(this).data("box-size");
-        updatePricingHTML(calculatePrice());
-        updateProductLink();
-
-        if (box_size == "small box") {
-          $('.box-carousel').slick('slickGoTo', 1);
-          $('#box-label').html(smallboxname);
-        } else if (box_size == "medium box") {
-          $('.box-carousel').slick('slickGoTo', 2);
-          $('#box-label').html(mediumboxname);
-        } else if (box_size == "starter box") {
-          $('.box-carousel').slick('slickGoTo', 0);
-          $('#box-label').html(starterboxname);
-        } else {
-          $('.box-carousel').slick('slickGoTo', 3);
-          $('#box-label').html(customboxname);
-        }
-      });
-    });
-
-    $(".frequency-selection").find(".btn-tab").each(function() {
-      $(this).click(function() {
-        $(".frequency-selection").find(".selected-tab").removeClass('selected-tab');
-        $(this).addClass('selected-tab');
-        delivery_frequency = $(this).html().replace("ly", "").trim();
-        updatePricingHTML(calculatePrice());
-        updateProductLink();
-      });
-    });
-
-  }
-}
-
 function boxSliderInit() {
     $('.box-carousel').slick({
       arrows: false,
@@ -333,7 +289,7 @@ function updateProductLink() {
   }
 }
 
-// Code by Websensenpro developers for new plans page
+// Code by Websensepro
 
 function updateProductLinkNew() {
   var link = $(".product-link-new");
@@ -382,6 +338,48 @@ function updateProductLinkNew() {
   }
 }
 
+var box_size = "starter box", delivery_frequency = "month", custom_box = false;
+function pricingFunctions() {
+  if ($('main').first().hasClass('pricing')) {
+    updatePricingHTML();
+    updateProductLinkNew();
+    boxSliderInit();
+    $(".size-selection").find(".btn-tab").each(function() {
+      $(this).click(function() {
+        $(".size-selection").find(".selected-tab").removeClass('selected-tab');
+        $(this).addClass('selected-tab');
+        box_size = $(this).data("box-size");
+        updatePricingHTML(calculatePrice());
+        updateProductLinkNew();
+
+        if (box_size == "small box") {
+          $('.box-carousel').slick('slickGoTo', 1);
+          $('#box-label').html(smallboxname);
+        } else if (box_size == "medium box") {
+          $('.box-carousel').slick('slickGoTo', 2);
+          $('#box-label').html(mediumboxname);
+        } else if (box_size == "starter box") {
+          $('.box-carousel').slick('slickGoTo', 0);
+          $('#box-label').html(starterboxname);
+        } else {
+          $('.box-carousel').slick('slickGoTo', 3);
+          $('#box-label').html(customboxname);
+        }
+      });
+    });
+
+    $(".frequency-selection").find(".btn-tab").each(function() {
+      $(this).click(function() {
+        $(".frequency-selection").find(".selected-tab").removeClass('selected-tab');
+        $(this).addClass('selected-tab');
+        delivery_frequency = $(this).html().replace("ly", "").trim();
+        updatePricingHTML(calculatePrice());
+        updateProductLinkNew();
+      });
+    });
+
+  }
+}
 
 function updatePricingHTML() {
   $(".frequency").html(delivery_frequency);

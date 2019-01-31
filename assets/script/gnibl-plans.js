82438af325c7988147e1151c56a950773a8db672
bilalnaseer01@gmail.com <bilalnaseer01@gmail.com>
2019-02-01 03:34:25 +1100

    Updated Gnibl Plans

--- assets/script/gnibl-plans.js
+++ assets/script/gnibl-plans.js
@@ -281,9 +281,9 @@ function updateProductLink() {
 }
 
 function updatePricingHTML() {
-  if (delivery_frequency.val() == "one off") {
+  // if (delivery_frequency.val() == "one off") {
   $(".frequency").html(delivery_frequency);  
-}  
+// }  
   $(".frequency-ly").html(delivery_frequency + "ly");
   $(".snack-num").html(snack_num);
   $(".per-snack").html("$" + (cost / snack_num).toFixed(2));
